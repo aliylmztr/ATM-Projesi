fb7e3b925aab008bb0e384b33a1182a679be7486
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-02-26 02:12:02 +0300

    ATM-Projesi

--- src/ATM_Projesi.java
+++ src/ATM_Projesi.java
@@ -1,12 +1,6 @@
-# ATM-Projesi
-Java ATM Projesi
-
-www.patika.dev
-
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String userName, password;
